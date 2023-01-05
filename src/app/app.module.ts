328bc6d7466af6f1f36c97f6c5916db63d3316e8
MAITITE MOHAMED <maititemohamed@gmail.com>
2023-01-05 16:09:33 +0100

    create component  Navbar and component   saibar with logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations:[
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent
+],
+
+imports:[BrowserModule,FormsModule,AppRoutingModule],
+
+
+
+bootstrap:[AppComponent] 
+
+
 })
-export class AppModule { }
+export class AppModule{}
